5e276464394bc2f97f9d884455810834ec4de123
mary <thoeun.mary@institute.pse.ngo>
2026-01-29 08:41:39 +0700

    done CD

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Express!"));
+app.get("/", (req, res) => res.send("Hello Express! CI-CD"));
 
 // Only start server if NOT in test mode
 if (process.env.NODE_ENV !== "test") {
